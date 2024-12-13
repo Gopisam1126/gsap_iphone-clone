0c5aff7201ffca8488af838720c66df9f927cac5
scriptKid <gopikrishnan0045@gmail.com>
2024-12-13 23:55:02 +0530

    file structure | navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "../src/assets/compStyles/common.css";
+import Navbar from "./assets/components/navbar";
+function App() {
+
+  return (
+    <>
+      <section className="main-section">
+        <Navbar/>
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/navbar.jsx
@@ -0,0 +1,33 @@
+import "../compStyles/navbar.css";
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import SearchIcon from '@mui/icons-material/Search';
+function Navbar() {
+  const navItems = ["iPhone", "iPad", "Mac", "Tablets", "Store", "Accessories", "Support", "Airpods"];
+
+  return (
+    <>
+      <header className="navbar-sec">
+        <nav className="navbar">
+          <img
+            src="\assets\images\apple.png"
+            alt="apple-icon"
+            id="apple-icon"
+          />
+          <div className="nav-items">
+            {navItems.map((item, i) => (
+              <ul key={i} className="nav-i-ul">
+                <li className="nav-i-li">{item}</li>
+              </ul>
+            ))}
+          </div>
+          <div className="action-icons">
+            <SearchIcon/>
+            <ShoppingBagIcon/>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default Navbar;
