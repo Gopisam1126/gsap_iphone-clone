9adcca6082e82d0b2f71e84450a6b30198bf11ec
scriptKid <gopikrishnan0045@gmail.com>
2025-01-05 14:25:23 +0530

    config user interactions

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Hero from "./assets/components/hero";
 import Highlights from "./assets/components/highlights";
 import Navbar from "./assets/components/navbar";
 import Model from "./assets/components/model";
+import * as Sentry from "@sentry/react";
 function App() {
+
   return (
     <>
       <section className="main-section">
@@ -16,4 +18,4 @@ function App() {
   );
 }
 
-export default App;
+export default Sentry.withProfiler(App);
